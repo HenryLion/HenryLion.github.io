4e5b987e60b39da81954e10e12a881765c051677
pencilhan <pencilhan@tencent.com>
2020-10-22 07:57:43 +0800

    my first web page

---
+++ first-website/mihoutao-site/scripts/main.js
@@ -0,0 +1,41 @@
+/* const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello World!'; */
+
+/* document.querySelector('html').onclick = function () {
+	alert('Ouch! Stop poking me!')
+} */
+
+let myImage = document.querySelector('img');
+myImage.onclick = function () {
+	let mySrc = myImage.getAttribute('src');
+	if (mySrc === 'images/mihoutao.jpg'){
+		myImage.setAttribute ('src','images/mihoutao-hua.jpg');
+		myImage.setAttribute ('width', 600);
+	} else {
+		myImage.setAttribute ('src','images/mihoutao.jpg');
+	}
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName () {
+	let myName = prompt ('Please enter your name.');
+	if (!myName){
+		setUserName();
+	} else {
+		localStorage.setItem ('name', myName);
+		myHeading.textContent = 'Mozilla is cool, ' + myName;		
+	}
+}
+
+if (!localStorage.getItem ('name')) {
+	setUserName();
+}else {
+	let storedName = localStorage.getItem('name');
+	myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+	setUserName();
+}
